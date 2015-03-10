dacbb885ad88ea29ce8346756160f3a52a5ea08a
goldcode101 <chris_lnz@yahoo.com>
2015-03-09 20:04:36 -0400

    Loading data(partial)

--- wxparse.py
+++ wxparse.py
@@ -3,11 +3,27 @@
 #To use: pass in the full absolute path to the file containing weather info to be parsed.
 import sys
 
+def LoadData(fileLines, headers):
+    print 'Loading data...'
+    print 'There are ' + str(len(fileLines)) + ' lines to be read.'
+
+    for ln in fileLines:
+        if(ln != fileLines[0]):
+            splitList = ln.split(',', len(headers))
+            for split in splitList:
+                day = []
+                day.append(split)
+    print len(day)
+    print 'Done!'
+
+
 def main(filename):
-    fileLines = open(filename, 'r').read().split('\n')
+    file = open(filename, 'r')
+    fileLines = file.read().split('\n')
     lstHeader = fileLines[0].split(',')
     print 'There are ' + str(len(lstHeader)) + ' headers.'
 
+    LoadData(fileLines, lstHeader)
 
 
 if __name__ == '__main__':
