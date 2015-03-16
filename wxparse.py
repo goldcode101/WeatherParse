e53e449c437647acf4451d116ad5ff5b34a21155
goldcode101 <chris_lnz@yahoo.com>
2015-03-15 20:39:10 -0400

    Finish loading data and start on types of queries

--- wxparse.py
+++ wxparse.py
@@ -7,15 +7,17 @@ def LoadData(fileLines, headers):
     print 'Loading data...'
     print 'There are ' + str(len(fileLines)) + ' lines to be read.'
 
+    arrayOfDays = []
     for ln in fileLines:
         if(ln != fileLines[0]):
             splitList = ln.split(',', len(headers))
+            day = []
             for split in splitList:
-                day = []
                 day.append(split)
-    print len(day)
+            arrayOfDays.append(day)
     print 'Done!'
 
+    return arrayOfDays
 
 def main(filename):
     file = open(filename, 'r')
@@ -23,7 +25,19 @@ def main(filename):
     lstHeader = fileLines[0].split(',')
     print 'There are ' + str(len(lstHeader)) + ' headers.'
 
-    LoadData(fileLines, lstHeader)
+    wxData = LoadData(fileLines, lstHeader)
+    invalidInput = True
+    while invalidInput:
+        queryType = raw_input('Select type of query: (D)ay, (S)ingle,')
+        if(queryType == 'd' or queryType == "D"):
+            invalidInput = False
+            #Do more day query stuff
+        elif(queryType == 's' or queryType == 'S'):
+            invalidInput = False
+            #Do more single query stuff
+        else:
+            invalidInput = True
+
 
 
 if __name__ == '__main__':
